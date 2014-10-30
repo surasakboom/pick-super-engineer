849c4a7f15fb2556e0ba41fda03d6e2e997ccd09
surasakboom <srs.ckjnk@gmail.com>
2014-10-30 01:49:00 -0300

    Code FeatureC

--- pick.rb
+++ pick.rb
@@ -12,3 +12,16 @@ name = pick_engineer()
 puts name						#print random name of engineer student(call 								function "pick_engineer()")
 puts featureA(name)					#puts return call function
 
+
+def feature_c(str)
+	allName = File.readlines("engineers.txt")
+	for i in 0..allName.size-1
+		for j in 0..allName[i].size-str.size
+			if allName[i][0..str.size-1].include? str
+			return allName[i]
+			end
+		end
+	end
+end
+
+puts feature_c("Surasak")
