adf3138985966cfcb94fc722f08b265ebc409751
saasbook <saasbook@saasbook.(none)>
2014-10-30 00:23:03 -0300

    first FeatureB

--- pick.rb
+++ pick.rb
@@ -1,7 +1,27 @@
-def pick_engineer()					#Create new function name "pick_engineer"
-  allName = File.readlines("engineers.txt")		#Create new variable "allname" to keep all name in text file
-	randName = allName[rand(allName.size)]		#Create new variable "randName" to keep random name of engineer student
-	return randName					#return random name of engineer student
+def pick_engineer()	#Create new function name "pick_engineer"
+  allName = File.readlines("engineers.txt")	#Create new variable "allname" to keep     all name in text file
+  randName = allName[rand(allName.size)]	#Create new variable "randName" to keep random name of engineer student
+  return randName	#return random name of engineer student
 end
 
-puts pick_engineer()					#print random name of engineer student(call function "pick_engineer()")
+def featureB(b)
+  count = 0
+  tot = ""
+  b.split("").each do |i|
+    tot = tot + i
+    if i == " "
+      count=count+1
+      next
+    end
+    if count == 1
+      tot = tot + "..."
+      return tot 
+    end
+  end 
+end
+
+a = pick_engineer()
+puts a
+puts featureB(a)	
+
+
